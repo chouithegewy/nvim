78cc3a93e1740c8ce7e110b26b45c5a2d29b8639
MPAdnuneschilds <dnunes-childs@motorcarparts.com>
2023-12-11 12:41:05 -0800

    php env

---
+++ lua/d/init.lua
@@ -0,0 +1,39 @@
+require("d.set")
+require("d.remap")
+
+local augroup = vim.api.nvim_create_augroup
+local ThePrimeagenGroup = augroup('d', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufWritePre"}, {
+    group = ThePrimeagenGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
+vim.filetype.add({
+    pattern = {
+        ['.*%.blade%.php'] = 'blade',
+    },
+})
