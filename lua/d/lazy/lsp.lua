61811553f8094b88d817c2b1540044a3f8f7ca8f
david <combatburrito@gmail.com>
2025-04-19 18:10:53 -0700

    jdtls

--- lua/d/lazy/lsp.lua
+++ lua/d/lazy/lsp.lua
@@ -126,9 +126,64 @@ return {
                         capabilities = capabilities,
                         settings = {
                             config = {
-                                cmd = { '/home/d/.local/share/nvim/mason/bin/jdtls' },
+                                cmd = {
+                                    -- 💀
+                                    'java', -- or '/path/to/java21_or_newer/bin/java'
+                                    -- depends on if `java` is in your $PATH env variable and if it points to the right version.
+
+                                    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+                                    '-Dosgi.bundles.defaultStartLevel=4',
+                                    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+                                    '-Dlog.protocol=true',
+                                    '-Dlog.level=ALL',
+                                    '-Xmx1g',
+                                    '--add-modules=ALL-SYSTEM',
+                                    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+                                    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+
+                                    -- 💀
+                                    '-jar', '/home/chilos/.local/share/nvim/mason/packages/jdtls/features/org.eclipse.equinox.executable_3.8.2800.v20241223-1657.jar',
+                                    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
+                                    -- Must point to the                                                     Change this to
+                                    -- eclipse.jdt.ls installation                                           the actual version
+
+
+                                    -- 💀
+                                    '-configuration', '/home/chilos/.local/share/nvim/mason/packages/jdtls/config_linux',
+                                    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
+                                    -- Must point to the                      Change to one of `linux`, `win` or `mac`
+                                    -- eclipse.jdt.ls installation            Depending on your system.
+
+
+                                    -- 💀
+                                    -- See `data directory configuration` section in the README
+                                    '-data', '~/projects/' .. vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+                                },
                                 root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+                                --root_dir = vim.fs.root(0, { ".git", "mvnw", "gradlew" }),
+
+                                -- Here you can configure eclipse.jdt.ls specific settings
+                                -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+                                -- for a list of options
+                                settings = {
+                                    java = {
+                                    }
+                                },
+
+                                -- Language server `initializationOptions`
+                                -- You need to extend the `bundles` with paths to jar files
+                                -- if you want to use additional eclipse.jdt.ls plugins.
+                                --
+                                -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
+                                --
+                                -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
+                                init_options = {
+                                    bundles = {}
+                                },
                             }
+                            -- This starts a new client & server,
+                            -- or attaches to an existing client & server depending on the `root_dir`.
+                            --require('jdtls').start_or_attach(config)
                         }
                     }
                 end,
